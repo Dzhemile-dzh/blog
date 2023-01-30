04177ed9f6bdc1f92584b67dbb2d21ae403599c0
djemile123 <cenito95@abv.bg>
2023-01-30 19:21:02 +0200

    fixed plain db password issue

--- config.php
+++ config.php
@@ -4,9 +4,9 @@
 return $config = 
 [
     'db' => [
-        'server'=>'localhost',
-        'dbname'=>'microblog',
-        'dbpass'=>'dzhemile42',
-        'dbuser'=>'dzhemile',
+        'server' => getenv('DB_SERVER'),
+        'dbname' => getenv('DB_NAME'),
+        'dbpass' => getenv('DB_PASSWORD'),
+        'dbuser' => getenv('DB_USER'),
     ],
 ];
